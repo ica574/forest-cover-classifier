3b46af792a67ac8a400c309119a7d74c5933b744
ica574 <iciliaat@gmail.com>
2022-12-19 18:20:42 +0100

    Renamed "x", and "y" to "data" and "labels".

--- preprocessing.py
+++ preprocessing.py
@@ -8,7 +8,7 @@ from sklearn.model_selection import train_test_split
 
 def load_data(): # Assimilates data from CSV file and seperates into data and labels
     data_frame = pd.read_csv("data/cover_data.csv") # Loads data from CSV file
-    return data_frame.iloc[:,:-1], data_frame.iloc[:,-1] # Returns data and labels respectively
+    return data_frame[["Elevation","Aspect","Slope","Horizontal_Distance_To_Hydrology","Vertical_Distance_To_Hydrology","Horizontal_Distance_To_Roadways","Hillshade_9am","Hillshade_Noon","Hillshade_3pm","Horizontal_Distance_To_Fire_Points","Wilderness_Area1","Wilderness_Area2","Wilderness_Area3","Wilderness_Area4","Soil_Type1","Soil_Type2","Soil_Type3","Soil_Type4","Soil_Type5","Soil_Type6","Soil_Type7","Soil_Type8","Soil_Type9","Soil_Type10","Soil_Type11","Soil_Type12","Soil_Type13","Soil_Type14","Soil_Type15","Soil_Type16","Soil_Type17","Soil_Type18","Soil_Type19","Soil_Type20","Soil_Type21","Soil_Type22","Soil_Type23","Soil_Type24","Soil_Type25","Soil_Type26","Soil_Type27","Soil_Type28","Soil_Type29","Soil_Type30","Soil_Type31","Soil_Type32","Soil_Type33","Soil_Type34","Soil_Type35","Soil_Type36","Soil_Type37","Soil_Type38","Soil_Type39","Soil_Type40"]], data_frame["class"] # Returns data and labels respectively
 
 data, labels = load_data() # Assigns data and labels to respective variables
-x_train, x_valid, y_train, y_valid = train_test_split(data, labels, test_size=0.20, stratify=labels, shuffle=True, random_state=22) # Splits data into training and validation data via scikit-learn
+data_train, data_valid, labels_train, labels_valid = train_test_split(data, labels, test_size=0.20, stratify=labels, shuffle=True, random_state=22) # Splits data into training and validation data via scikit-learn
